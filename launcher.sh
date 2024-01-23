614c777220e67348c637d9d5f9a9aaee39ff05f1
Jonas Legion <jturbeaux@pm.me>
2024-01-23 16:43:39 +0100

    sleep

--- launcher.sh
+++ launcher.sh
@@ -10,10 +10,10 @@ echo "Poetry install ok"
 #sleep infinity
 
 poetry run python3 manage.py migrate
+poetry run python3 manage.py install
 poetry run python3 manage.py collectstatic --noinput
-
 #echo "Run GUNICORN"
-poetry run gunicorn fedowallet_django.wsgi --log-level=info --log-file /home/fedow/Fedow/logs/gunicorn.logs -w 5 -b 0.0.0.0:8000
+poetry run gunicorn fedowallet_django.wsgi --log-level=debug --log-file /home/fedow/Fedow/logs/gunicorn.logs -w 5 -b 0.0.0.0:8000
 
 # echo "sleep"
 #sleep infinity
