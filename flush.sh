935966bbaac984bb77e5945690ebc9f2a28f408f
Jonas Legion <jturbeaux@pm.me>
2024-05-04 18:10:19 +0200

    flush no input

--- flush.sh
+++ flush.sh
@@ -6,13 +6,13 @@ set -e
 # /bin/bash
 if [ "$TEST" = "1" ]
 then
+  poetry run ./manage.py migrate
   poetry run ./manage.py flush --no-input
   poetry run ./manage.py install
   poetry run ./manage.py createsuperuser --noinput --username root --email root@root.root
   poetry run ./manage.py shell_plus -c "User=get_user_model();root=User.objects.get(username='root');root.set_password('root');root.save()"
   echo "start dev server : https://fedow.tibillet.localhost/dashboard/"
   poetry run ./manage.py runserver 0.0.0.0:8000
-
 else
     echo "TEST environment variable is not set"
 fi
