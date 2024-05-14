eb9049d88e4edc46a2659ce52a4918850da1d3d8
Jonas Legion <jturbeaux@pm.me>
2024-05-14 08:57:04 +0200

    domain in flush

--- flush.sh
+++ flush.sh
@@ -3,7 +3,7 @@ set -e
 # test si ça crash bien :
 #cat non_existent_file.txt
 #echo "devrait pas passer"
-# /bin/bash
+
 if [ "$TEST" = "1" ]
 then
   poetry run ./manage.py migrate
@@ -12,7 +12,7 @@ then
   poetry run ./manage.py demo_data
   poetry run ./manage.py createsuperuser --noinput --username root --email root@root.root
   poetry run ./manage.py shell_plus -c "User=get_user_model();root=User.objects.get(username='root');root.set_password('root');root.save()"
-  echo "start dev server : https://fedow.tibillet.localhost/dashboard/"
+  echo "start dev server : https://$DOMAIN/dashboard/"
   poetry run ./manage.py runserver 0.0.0.0:8000
 else
     echo "TEST environment variable is not set"
