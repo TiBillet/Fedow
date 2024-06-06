6a788db4b3071db74f5047dc891350fa455be6b8
Jonas Legion <jturbeaux@pm.me>
2024-06-06 14:53:33 +0200

    scrip import card

--- fedow_core/management/commands/import_cards.py
+++ fedow_core/management/commands/import_cards.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
 from django.core.validators import URLValidator
 
-from fedow_core.models import Place
+from fedow_core.models import Place, Card, Origin
 
 
 class Command(BaseCommand):
@@ -26,11 +26,13 @@ class Command(BaseCommand):
         file = open(input_fichier_csv)
 
         csv_parser = csv.reader(file)
-        list_card = []
+        list_cards = []
         for line in csv_parser:
             url_qrcode = line[0]
             if not self.is_string_an_url(url_qrcode):
                 raise Exception('Url qrcode must be a valid url')
+            qr_code = UUID(url_qrcode.partition('/qr/')[2])
+
             number = line[1].upper()
             if not type(number) == str or not len(number) == 8:
                 raise Exception('Printed number must be 8 len string')
@@ -39,10 +41,10 @@ class Command(BaseCommand):
                 raise Exception('Tag id must be 8 len string')
 
             print(f"url_qrcode: {url_qrcode}, number: {number}, tag_id: {tag_id}")
-            list_card.append((tag_id, number, url_qrcode))
+            list_cards.append((qr_code, number, tag_id))
         file.close()
 
-        print(f'{len(list_card)} card to import')
+        print(f'{len(list_cards)} card to import')
         print(f"Please select the origin place :")
         places = [place.name for place in Place.objects.all()]
         for index, place_name in enumerate(places):
@@ -51,5 +53,22 @@ class Command(BaseCommand):
         choosen_place_name = places[int(indexplace)]
         place: Place = Place.objects.get(name=choosen_place_name)
 
-        import ipdb; ipdb.set_trace()
+        generation_nbr = input('\nEnter a generation number \n')
+        generation_nbr = int(generation_nbr)
 
+        origin, created = Origin.objects.get_or_create(
+            place=place,
+            generation=generation_nbr
+        )
+
+        for card in list_cards:
+            qr_code = card[0]
+            number = card[1]
+            tag_id = card[2]
+            print(f"Create : qr_code: {qr_code}, number: {number}, tag_id: {tag_id}")
+            Card.objects.get_or_create(
+                qrcode_uuid=qr_code,
+                number_printed=number,
+                first_tag_id=tag_id,
+                origin=origin
+            )
