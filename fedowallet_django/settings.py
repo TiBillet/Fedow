6af28745e6284a0a23de0fe1e378fe7b15266afa
JonasFW13 <jturbeaux@pm.me>
2026-07-17 18:15:18 +0200

    Adjust `ALLOWED_HOSTS` and `CSRF_TRUSTED_ORIGINS` to include `fedow` subdomain in non-`DEBUG` mode.

--- fedowallet_django/settings.py
+++ fedowallet_django/settings.py
@@ -39,9 +39,10 @@ TEST = os.environ.get('TEST') == 'True' or os.environ.get('TEST') == '1'
 ALLOWED_HOSTS = [os.environ['DOMAIN'], ]
 ALLOWED_HOSTS.append(f"fedow.{os.environ['DOMAIN']}")
 CSRF_TRUSTED_ORIGINS = [f"https://{os.environ['DOMAIN']}", ]
+CSRF_TRUSTED_ORIGINS.append(f"https://fedow.{os.environ['DOMAIN']}")
+
 if DEBUG:
     ALLOWED_HOSTS.append('127.0.0.1')
-    CSRF_TRUSTED_ORIGINS.append(f"https://fedow.{os.environ['DOMAIN']}")
 
 # Sentry
 if not DEBUG:
